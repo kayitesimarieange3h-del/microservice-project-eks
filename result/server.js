c4e9daba09a0c5abfca6d0a0c9634183c6e2e541
public00 <lunicvedran@gmail.com>
2025-12-12 12:28:57 +1100

    change db to statefull set

--- result/server.js
+++ result/server.js
@@ -2,20 +2,21 @@ var express = require('express'),
     async = require('async'),
     { Pool } = require('pg'),
     cookieParser = require('cookie-parser'),
+    path = require('path'), // <--- FIX: Added missing import
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server);
 
-    const winston = require('winston');
-    const logger = winston.createLogger({
-      format: winston.format.json(),
-      transports: [new winston.transports.Console()],
-    });
+// Setup Winston Logger (JSON format)
+const winston = require('winston');
+const logger = winston.createLogger({
+  format: winston.format.json(),
+  transports: [new winston.transports.Console()],
+});
 
 var port = process.env.PORT || 4000;
 
 io.on('connection', function (socket) {
-
   socket.emit('message', { text : 'Welcome!' });
 
   socket.on('subscribe', function (data) {
@@ -23,8 +24,11 @@ io.on('connection', function (socket) {
   });
 });
 
+// Use Environment Variable for DB connection (Senior SRE Best Practice)
+var connectionString = process.env.POSTGRES_CONNECTION_STRING || 'postgres://postgres:postgres@db/postgres';
+
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: connectionString
 });
 
 async.retry(
@@ -32,16 +36,19 @@ async.retry(
   function(callback) {
     pool.connect(function(err, client, done) {
       if (err) {
-        console.error("Waiting for db");
+        // FIX: Use logger instead of console.error
+        logger.error("Waiting for db", { error: err.message });
       }
       callback(err, client);
     });
   },
   function(err, client) {
     if (err) {
-      return console.error("Giving up");
+      // FIX: Use logger
+      return logger.error("Giving up", { error: err.message });
     }
-    logger.info("Connected to database", { service: "result-app", attempt: retryCount });
+    // FIX: Removed undefined 'retryCount' variable
+    logger.info("Connected to database", { service: "result-app" });
     getVotes(client);
   }
 );
@@ -49,13 +56,14 @@ async.retry(
 function getVotes(client) {
   client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
     if (err) {
-      console.error("Error performing query: " + err);
+      // FIX: Use logger
+      logger.error("Error performing query", { error: err.message });
     } else {
       var votes = collectVotesFromResult(result);
       io.sockets.emit("scores", JSON.stringify(votes));
     }
 
-    setTimeout(function() {getVotes(client) }, 1000);
+    setTimeout(function() { getVotes(client) }, 1000);
   });
 }
 
@@ -70,7 +78,8 @@ function collectVotesFromResult(result) {
 }
 
 app.use(cookieParser());
-app.use(express.urlencoded());
+// FIX: Added extended option to silence deprecation warning
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
@@ -79,5 +88,6 @@ app.get('/', function (req, res) {
 
 server.listen(port, function () {
   var port = server.address().port;
-  console.log('App running on port ' + port);
+  // FIX: Use logger
+  logger.info('App running', { port: port });
 });
